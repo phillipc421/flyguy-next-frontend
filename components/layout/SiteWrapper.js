1b78a9e0aa411347c242353343ee1a88b42a9223
Phillip <phillip@phillipclark.dev>
2023-04-21 17:29:42 -0500

    cart header button

--- components/layout/SiteWrapper.js
+++ components/layout/SiteWrapper.js
@@ -1,3 +1,6 @@
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
 import Header from "./Header";
 import Footer from "./Footer";
 import styles from "./SiteWrapper.module.css";
@@ -6,6 +9,11 @@ export default function SiteWrapper({ children }) {
 		<main className={styles.container}>
 			<Header>
 				<p>FLYGUY Hair</p>
+				<IconButton onClick={() => console.log("cart")} color="primary">
+					<Badge badgeContent={4}>
+						<ShoppingCartIcon fontSize="large"></ShoppingCartIcon>
+					</Badge>
+				</IconButton>
 			</Header>
 			{children}
 			<Footer>
