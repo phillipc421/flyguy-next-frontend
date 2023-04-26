a4b80398f60caa225b3b829a664cae7d33f91e24
Phillip Clark <phillipclark@Phillips-MBP.attlocal.net>
2023-04-26 00:05:28 -0500

    create product page

--- pages/_app.js
+++ pages/_app.js
@@ -4,26 +4,26 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import SiteWrapper from "../components/layout/SiteWrapper";
-import "../styles/globals.css";
+// import "../styles/globals.css";
 
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: "#3c3c3c",
-		},
-		secondary: {
-			main: "#ffffff",
-		},
-	},
+  palette: {
+    primary: {
+      main: "#3c3c3c",
+    },
+    secondary: {
+      main: "#ffffff",
+    },
+  },
 });
 function MyApp({ Component, pageProps }) {
-	return (
-		<ThemeProvider theme={theme}>
-			<SiteWrapper>
-				<Component {...pageProps} />
-			</SiteWrapper>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <SiteWrapper>
+        <Component {...pageProps} />
+      </SiteWrapper>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
