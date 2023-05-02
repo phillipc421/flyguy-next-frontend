241aaca51f5daa07bc4071edd4f2e567f1e49018
madmonkey <104393122+phillipc421@users.noreply.github.com>
2023-05-02 16:24:15 -0500

    checkout endpoint and stipe package

---
+++ components/checkout/Checkout.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { IconButton } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+export default function Checkout({ cart, checkoutSetter }) {
+  const [stripeCs, setStripeCs] = useState("");
+  useEffect(() => {
+    const fetcher = async () => {
+      try {
+        const response = await fetch("/api/checkout", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(cart),
+        });
+        const data = await response.json();
+        if (data.stripeClientSecret) {
+          setStripeCs(data.stripeClientSecret);
+        }
+        // success
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    if (!stripeCs) {
+      fetcher();
+    }
+    return () => console.log("unmounted");
+  }, [cart]);
+  console.log(stripeCs);
+  return (
+    <div>
+      {/* {JSON.stringify(cart)} */}
+      <IconButton onClick={() => checkoutSetter(false)}>
+        <ArrowBackIcon></ArrowBackIcon>
+      </IconButton>
+    </div>
+  );
+}
